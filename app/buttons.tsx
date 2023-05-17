1548c9047ecec2e18606fa3f6430d2c27a9f0366
anniefangg <anniefangg@gmail.com>
2023-05-17 19:18:17 +0100

    We added a page for buttons

---
+++ app/buttons.tsx
@@ -0,0 +1,11 @@
+
+import { Button } from "@/devlink"
+
+export default function Buttons() {
+  return (
+    <div>
+        <p>This is where all the buttons will be </p>
+    <Button/>
+    </div>
+  )
+}
